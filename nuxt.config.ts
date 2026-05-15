78eb088803f347f1720791d6819ea5f7f06c0c8d
Dan Pastori <dan@521dimensions.com>
2026-05-14 22:32:14 -0500

    Installed and configured nuxt seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxt/fonts', '@nuxt/image'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxtjs/seo',
+  ],
   css: ['~/assets/css/main.css'],
   fonts: {
     families: [
